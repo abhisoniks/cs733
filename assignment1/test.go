4b2f148f46629f5e4673b018d33e70737169a6fd
abhisoniks <abhisoni@cse.iitb.ac.in>
2016-01-13 01:22:12 +0530

    code added

---
+++ assignment1/test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "fmt"
+    "net"
+    "os"
+)
+func main() {
+    service := ":8080"
+    tcpAddr,err := net.ResolveTCPAddr("tcp", service)
+    checkError(err)
+    listener,err := net.ListenTCP("tcp",tcpAddr)
+    checkError(err)
+    for {
+        conn, err := listener.Accept()
+        if err != nil {
+            continue
+        }
+        sss := "Hello"
+	fmt.Println("Hello")
+        conn.Write([]byte(sss))
+fmt.Println("Hello")
+        conn.Close()                // we're finished with this client
+    }
+}
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
